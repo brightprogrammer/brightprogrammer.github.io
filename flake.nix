5c4506eb16fdd1eb81ce2da54618fd3325e3397c
Siddharth Mishra <admin@brightprogrammer.in>
2026-05-01 14:28:10 -0700

    Try Removing Dart Sass

--- flake.nix
+++ flake.nix
@@ -5,39 +5,50 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in
     {
-      devShells = forAllSystems (system:
+      devShells = forAllSystems (
+        system:
         let
           pkgs = import nixpkgs { inherit system; };
           hugoPkg =
-            if builtins.hasAttr "hugo-extended" pkgs then pkgs."hugo-extended"
-            else if builtins.hasAttr "hugoExtended" pkgs then pkgs.hugoExtended
-            else pkgs.hugo;
+            if builtins.hasAttr "hugo-extended" pkgs then
+              pkgs."hugo-extended"
+            else if builtins.hasAttr "hugoExtended" pkgs then
+              pkgs.hugoExtended
+            else
+              pkgs.hugo;
         in
         {
           default = pkgs.mkShell {
             packages = [
               hugoPkg
-              pkgs.nodejs
+              # pkgs.nodejs
               pkgs.git
             ];
             shellHook = ''
-              if [ ! -d "node_modules" ]; then
-                echo "Installing npm dependencies..."
-                if [ -f "package-lock.json" ]; then
-                  npm ci
-                else
-                  npm install
-                fi
-              fi
-              echo "brightprogrammer.github.io dev shell ready (hugo + nodejs/npm)"
+              # if [ ! -d "node_modules" ]; then
+              #   echo "Installing npm dependencies..."
+              #   if [ -f "package-lock.json" ]; then
+              #     npm ci
+              #   else
+              #     npm install
+              #   fi
+              # fi
+              # echo "brightprogrammer.github.io dev shell ready (hugo + nodejs/npm)"
             '';
           };
-        });
+        }
+      );
     };
 }
